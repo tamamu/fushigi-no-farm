e0b109065103dfc6813381645247bd69e3fbd449
tamamu <tamamu.1r1s@gmail.com>
2016-08-30 17:53:11 +0900

    enable browserify

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,39 +1,43 @@
-import gulp from 'gulp'
-import gulpLoadPlugins from 'gulp-load-plugins'
+import gulp from 'gulp';
+import gulpLoadPlugins from 'gulp-load-plugins';
+import browserify from 'browserify';
+import babelify from 'babelify';
+import source from 'vinyl-source-stream';
 
-const $ = gulpLoadPlugins()
+const $ = gulpLoadPlugins();
 
 const targets = [
-	'src/scripts/**/*.js',
-	'index.html',
+  'src/*.js',
+  'index.html',
 ];
 
-gulp.task('babel', () => 
-	gulp.src('src/scripts/**/*.js')
-		.pipe($.plumber())
-		.pipe($.sourcemaps.init())
-		.pipe($.babel())
-		.pipe($.sourcemaps.write("."))
+gulp.task('babel', () =>
+	browserify('src/main.js', {debug: true})
+		.transform(babelify)
+		.bundle()
+		.on('error', (err) => console.log('Error : ' + err.message))
+		.pipe(source('main.js'))
+		//.pipe($.plumber())
+		//.pipe($.sourcemaps.init())
+		//.pipe($.babel())
+		//.pipe($.sourcemaps.write('.'))
 		.pipe(gulp.dest('dist'))
-)
+);
 
 gulp.task('watch', () =>
-	gulp.watch('src/scripts/**/*.js', ['babel', 'reload'])
-)
+	gulp.watch('src/*.js', ['babel', 'reload'])
+);
 
-gulp.task('webserver', () =>
-	gulp.src('./')
-		.pipe($.webserver({
-			host: 'localhost',
-			port: 8000,
-			livereload: true,
-			open: true
-		}))
-)
+gulp.task('webserver', () => gulp.src('./').pipe($.webserver({
+  host: 'localhost',
+  port: 8000,
+  livereload: true,
+  open: true
+})));
 
 gulp.task('reload', () =>
 	gulp.src(targets)
 		.pipe($.webserver.reload())
-)
+);
 
-gulp.task('default', ['webserver', 'watch'])
+gulp.task('default', ['webserver', 'watch']);
